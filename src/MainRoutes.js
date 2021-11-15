70b115dc89c0acdb5e6f5141fc1714609400c794
Shirin09 <91154330+Shirin09@users.noreply.github.com>
2021-11-15 12:34:32 +0600

    add AuthContext

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import AuthContextProvider from "./context/AuthContext";
+
 
 const MainRoutes = () => {
   return (
+    <AuthContextProvider>
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+
       </Routes>
     </BrowserRouter>
+    </AuthContextProvider>
   );
 };
 
