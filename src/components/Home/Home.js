743b15168f110cffdd98e4e3bf3bfaef01911000
Synjy <aisalkyn.baizakova@gmail.com>
2021-11-15 11:18:00 +0500

    gg

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+import Products from "../Products/Products";
+
+const Home = () => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Products />
+    </div>
+  );
+};
+
+export default Home;
