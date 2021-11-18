6893d99b6194eef16752e0ea55af444200417055
Shirin09 <91154330+Shirin09@users.noreply.github.com>
2021-11-18 17:26:54 +0600

    ,

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,13 @@ import Products from "../Products/Products";
 import MainCarousel from "../Carousel/Carousel";
 import "./Home.css";
 import RangeSlider from "./RangeSlider/RangeSlider";
+import Content from "../Content.js/Content";
 
 const Home = () => {
   return (
     <div className="wrapper">
       <MainCarousel />
+      <Content />
       <RangeSlider />
       <Products />
     </div>
