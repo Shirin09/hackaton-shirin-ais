42c14396dced2f88c41682a5fd05bbca9d886e39
Synjy <aisalkyn.baizakova@gmail.com>
2021-11-19 13:15:36 +0500

    add styles to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,55 +12,57 @@ const Cart = () => {
   }, []);
 
   return (
-    <div className="cart">
-      {cart.products ? (
-        <div>
-          <div className="cart">
-            <table>
-              <thead>
-                <tr>
-                  <th>Image</th>
-                  <th>Name</th>
-                  <th>Price</th>
-                  <th>Count</th>
-                  <th>SubPrice</th>
-                </tr>
-              </thead>
-              <tbody>
-                {cart.products.map((elem) => (
-                  <tr key={elem.item.id}>
-                    <td>
-                      <img src={elem.item.image} alt="product img" />
-                    </td>
-                    <td>{elem.item.name}</td>
-                    <td>{elem.item.price}</td>
-                    <td>
-                      <input
-                        min="0"
-                        value={elem.count}
-                        type="number"
-                        onChange={(e) =>
-                          changeProductCount(e.target.value, elem.item.id)
-                        }
-                      />
-                    </td>
-                    <td>{elem.subPrice}</td>
+    <center>
+      <div className="cart">
+        {cart.products ? (
+          <div className="main-cart">
+            <div className="cart">
+              <table className="styled-table">
+                <thead>
+                  <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Count</th>
+                    <th>SubPrice</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-            <h4>Total: {cart.totalPrice} </h4>\
-            <div>
-              <Link to="/cart/cashout">
-                <button>Buy</button>
-              </Link>
+                </thead>
+                <tbody>
+                  {cart.products.map((elem) => (
+                    <tr key={elem.item.id}>
+                      <td>
+                        <img src={elem.item.image} alt="product img" />
+                      </td>
+                      <td>{elem.item.name}</td>
+                      <td>{elem.item.price}</td>
+                      <td>
+                        <input
+                          min="0"
+                          value={elem.count}
+                          type="number"
+                          onChange={(e) =>
+                            changeProductCount(e.target.value, elem.item.id)
+                          }
+                        />
+                      </td>
+                      <td>{elem.subPrice}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <h4>Total: {cart.totalPrice} </h4>
+              <div>
+                <Link to="/cart/cashout">
+                  <button>Buy</button>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <h1>loading</h1>
-      )}
-    </div>
+        ) : (
+          <h1>loading</h1>
+        )}
+      </div>
+    </center>
   );
 };
 
